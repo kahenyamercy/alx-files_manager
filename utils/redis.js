98a22f5e3237ae274dc62ce049ac1bb8e3b6190d
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-12 11:54:46 +0300

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,44 +1,44 @@
 const redis = require('redis');
 
-
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor() {
+    this.client = redis.createClient();
 
-        this.client.on('error', (err) => {
-            console.error('Redis Client Error:', err);
-        });
-    }
-    isAlive() {
-        return this.client.connected;
-    }
+    this.client.on('error', (err) => {
+      console.error('Redis Client Error:', err);
+    });
+  }
 
-    async get(key) {
-        try {
-            const value = await this.client.get(key);
-            return value;
-        } catch (error) {
-            console.error('Error getting value in Redis:', error);
-            return null;
-        }
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async set(key, value, duration) {
-        try {
-            await this.client.set(key, value, 'EX', duration);
-        } catch (error) {
-            console.error('Error setting value in Redis:', error);
-        }
+  async get(key) {
+    try {
+      const value = await this.client.get(key);
+      return value;
+    } catch (error) {
+      console.error('Error getting value from Redis:', error);
+      return null;
     }
+  }
 
-    async del(key) {
-        try {
-            await this.client.del(key);
-        } catch (error) {
-            console.error('Error deleting key from Redis:', error);
-        }
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, 'EX', duration);
+    } catch (error) {
+      console.error('Error setting value in Redis:', error);
     }
+  }
+
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (error) {
+      console.error('Error deleting key from Redis:', error);
+    }
+  }
 }
 
-const redisClient = newRedisClient();
+const redisClient = new RedisClient();
 module.exports = redisClient;
