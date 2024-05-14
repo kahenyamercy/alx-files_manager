867c5745932d37f21fc4fc21e3cb27e8394a2d89
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-15 00:22:14 +0300

    First API

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
+import express from 'express';
+import { env } from 'process';
+
+const mainRoute = require('./routes/index');
+
 const app = express();
-const port = process.env.PORT || 5000;
-const routes = require('./routes');
+const port = env.PORT || 5000;
+app.use(express.json());
+app.use(mainRoute);
+app.listen(port, '127.0.0.1');
 
-app.use('/', routes);
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+export default app;
